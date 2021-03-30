1a69ff6ba0de6c39470b8af0037554998624064e
Andrew Grindstaff <ajgrinds03@gmail.com>
2021-03-29 18:21:55 -0700

    Update pwm.py

--- pwm.py
+++ pwm.py
@@ -1,6 +1,6 @@
 # Change these
-pin = 12
-frequencyHz = 1500
+pin = 33
+frequencyHz = 1000
 
 import RPi.GPIO as GPIO
 import time
@@ -9,14 +9,14 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin, GPIO.OUT)
 pwm = GPIO.PWM(pin, frequencyHz)
 
-dc = input("Duty cycle: ")
+dc = int(input("Duty cycle: "))
 pwm.start(dc)
 
 try:
     while True:
         pwm.ChangeDutyCycle(dc)
         time.sleep(0.05)
-        dc = input("Duty cycle: ")
+        dc = int(input("Duty cycle: "))
 except KeyboardInterrupt:
     print("Stopping")
 
